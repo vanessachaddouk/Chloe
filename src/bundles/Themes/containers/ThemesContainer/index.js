aa84a82c772a979c421c87a51376da5bdda8ca9c
Charles Mangwa <charlesmangwa@gmail.com>
2016-12-10 19:15:43 +0100

    feat(Themes): clean general UI

--- src/bundles/Themes/containers/ThemesContainer/index.js
+++ src/bundles/Themes/containers/ThemesContainer/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, { Component } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import Title from '@components/Title'
 import Tile from '@components/Tile'
 import styles from './styles'
@@ -15,19 +15,28 @@ class ThemesContainer extends Component {
   state: State
   render() {
     return (
-      <ScrollView
-        bounces={false}
-        style={styles.container}
-      >
-        <Title size="big">Chloé</Title>
-        <Tile
-          image="https://i.ytimg.com/vi/QrOQiknOE9w/maxresdefault.jpg"
-          period="middleAge"
-          stories={3}
-          title="Les grottes de Lascaux"
-          description="Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! I’m the best thing!"
-        />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.scrollview}
+        >
+          <Title size="big">Chloé</Title>
+          <View style={styles.wrapper}>
+            {Array.from({ length: 6 }, (a, index) => index).map((item) => (
+              <View key={item} style={styles.tile}>
+                <Tile
+                  image="https://i.ytimg.com/vi/QrOQiknOE9w/maxresdefault.jpg"
+                  period="preHistory"
+                  stories={3}
+                  title="Les grottes de Lascaux"
+                  description="Look at that text! Would anyone use that? Can you imagine that, the text of your next webpage?! I’m the best thing!"
+                />
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
     )
   }
 }
--- src/bundles/Themes/containers/ThemesContainer/styles.js
+++ src/bundles/Themes/containers/ThemesContainer/styles.js
@@ -9,7 +9,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: DEFAULT_BACKGROUND_COLOR,
     paddingHorizontal: rem(1.25),
-    paddingVertical: rem(3.125),
+    paddingTop: rem(3.125),
+  },
+  wrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  tile: {
+    marginBottom: rem(3.125),
   },
 })
 
